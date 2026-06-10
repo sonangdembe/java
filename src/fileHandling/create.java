dcb16ffccd70231aed5da3eb6b05bff9a45cf326
Sona Angdembe <asonaxaviersm1@gmail.com>
2026-06-10 09:35:50 -0400

    create and write in file

--- src/fileHandling/create.java
+++ src/fileHandling/create.java
@@ -1,4 +1,22 @@
 package fileHandling;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.FileWriter;
 public class create {
+    public static void main(String[] args){
+        try{
+            File myfile = new File("newFile.txt");
+            if(myfile.createNewFile()){
+                System.out.println("File created successfully " + myfile.getName());
+           }
+            FileWriter myWrite = new FileWriter("newFile.txt");
+            myWrite.write("hello this is for the create");
+            myWrite.close();
+            System.out.println("File written to " + myfile.getName());
+
+        }catch (IOException e){
+            System.out.println("File could not be created");
+        }
+    }
 }

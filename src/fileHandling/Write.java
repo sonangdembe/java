dea03a61ed398731e13480110061ec2a06d35e3f
Sona Angdembe <asonaxaviersm1@gmail.com>
2026-06-10 09:35:22 -0400

    Write in the file

--- src/fileHandling/Write.java
+++ src/fileHandling/Write.java
@@ -1,4 +1,13 @@
 package fileHandling;
 
+import java.io.FileWriter;
 public class Write {
+    public static void main(String[] args){
+        try(FileWriter myWriter  = new FileWriter("Hello.txt", true)){
+            myWriter.write("Hi how are you \nmy name is Edha how it's going");
+            System.out.println("File written successfully");
+        }catch(Exception e){
+            System.out.println("Error");
+        }
+    }
 }

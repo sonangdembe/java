455183519467619b08c0666f359abab55491fc64
Sona Angdembe <asonaxaviersm1@gmail.com>
2026-06-10 08:22:30 -0400

    number guessing added

--- src/numberGuessing/Main.java
+++ src/numberGuessing/Main.java
@@ -1,4 +1,39 @@
 package numberGuessing;
 
-public class Main {
+
+import java.util.Scanner;
+import java.util.Random;
+
+class RanNum {
+    int ranNum;
+
+
+    public int getRanNum() {
+        Random random = new Random();
+        ranNum = random.nextInt(10) + 1;
+        return ranNum;
+    }
+
+}
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        RanNum rn = new RanNum();
+        int secretNumber = rn.getRanNum();
+        while(true){
+
+            System.out.println("Enter a number");
+            int num = sc.nextInt();
+
+            if (secretNumber == num) {
+                System.out.println("You guessed the correct number");
+break;
+            }else if(secretNumber >= num){
+                System.out.println("You guessed the smalll number");
+            }else{
+                System.out.println("You guessed the biggest number");
+            }
+        }
+    }
 }

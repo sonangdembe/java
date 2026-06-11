4f56fe9f7a796bebc8640b4b5b3653c17ae74a40
Sona Angdembe <asonaxaviersm1@gmail.com>
2026-06-11 07:49:23 -0400

    search word added

--- src/fileHandling/Read.java
+++ src/fileHandling/Read.java
@@ -7,14 +7,27 @@ import java.util.Scanner;
 
 public class Read {
     public static void main(String[] args){
-        int count = 0;
+
         File myObj = new File("Hello.txt");
+        System.out.println("Enter the word you want to search");
+        String searchWord = new Scanner(System.in).next();
+         boolean found = false;
+
         try(Scanner myReader = new Scanner(myObj)){
-         while(myReader.hasNextLine()){
-            count++;
-            System.out.println(myReader.nextLine());
+         while(myReader.hasNext()) {
+             String word = myReader.next();
+             if (searchWord.equalsIgnoreCase(word)) {
+                 found = true;
+                 break;
+             }
          }
-            System.out.println(count);
+          if(found){
+              System.out.println("found");
+          }else{
+              System.out.println("not found");
+          }
+
+
         }catch(FileNotFoundException e){
             System.out.println("File not found");
         }

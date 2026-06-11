28d2b447834fd54af54d9dfe530c4364acedd43a
Sona Angdembe <asonaxaviersm1@gmail.com>
2026-06-11 07:01:21 -0400

    count words added

--- src/fileHandling/Read.java
+++ src/fileHandling/Read.java
@@ -7,12 +7,14 @@ import java.util.Scanner;
 
 public class Read {
     public static void main(String[] args){
+        int count = 0;
         File myObj = new File("Hello.txt");
         try(Scanner myReader = new Scanner(myObj)){
-         while(myReader.hasNextLine()){
-
-             System.out.println(myReader.nextLine());
+         while(myReader.hasNext()){
+            count++;
+            System.out.println(myReader.next());
          }
+            System.out.println(count);
         }catch(FileNotFoundException e){
             System.out.println("File not found");
         }

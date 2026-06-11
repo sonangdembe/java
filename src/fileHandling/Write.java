bcc39fc888dee5f2259b0138ba0d062eed11bd32
Sona Angdembe <asonaxaviersm1@gmail.com>
2026-06-11 08:02:26 -0400

    added name in file

--- src/fileHandling/Write.java
+++ src/fileHandling/Write.java
@@ -1,10 +1,18 @@
 package fileHandling;
 
 import java.io.FileWriter;
+import java.util.Scanner;
+
 public class Write {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
         try(FileWriter myWriter  = new FileWriter("Hello.txt", true)){
-            myWriter.write("Hi how are you \nmy name is Edha how it's going");
+
+            System.out.println("Enter the name of student u want to add");
+            String name = sc.nextLine();
+
+            myWriter.write(name + "\n");
+
             System.out.println("File written successfully");
         }catch(Exception e){
             System.out.println("Error");

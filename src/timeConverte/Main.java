7c29244e66eb38fbe345cc339639ab8e97aa86c1
Sona Angdembe <asonaxaviersm1@gmail.com>
2026-06-21 09:18:27 -0400

    display time according to the user input

--- src/timeConverte/Main.java
+++ src/timeConverte/Main.java
@@ -1,19 +1,32 @@
 package timeConverte;
+import java.util.Scanner;
 
-class converter{
-    void intoMin(){
-        int minTime = 24*60;
-        System.out.println("minTime:"+minTime);
-    }
-    void intoSec(){
-        int secTime = 24*60*60;
-        System.out.println("secTime:"+secTime);
-    }
-}
 public class Main {
-    public static void main(String[] args){
-     converter convertTime = new converter();
-     convertTime.intoMin();
-        convertTime.intoSec();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the time how many days");
+        int time = sc.nextInt();
+
+        while (true) {
+            System.out.println("Enter 1 to change into min");
+            System.out.println("Enter 2 to change into sec");
+            System.out.println("Enter your choice");
+
+            int choice = sc.nextInt();
+            sc.nextLine();
+            switch(choice) {
+                case 1:
+                    int minTime = time*24*60;
+                    System.out.println("minTime:"+minTime);
+
+                break;
+                case 2:
+                    int secTime = time*24*60* 60;
+                    System.out.println("secTime:"+secTime);
+                default:
+                System.out.println("Invalid choice");
+            }
+        }
     }
 }
